7e2dd64386c3e206a5ae37fe9ab2cfc773bef597
Katie Otto <33523737+kateotto@users.noreply.github.com>
2020-06-17 19:15:51 +0200

    Changes in  isPeselValid function

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -233,7 +233,7 @@ export default {
       );
     },
     isPeselValid(pesel) {
-      let weight = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
+      const weight = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
       let sum = 0;
       let controlNumber = parseInt(pesel.substring(10, 11));
       for (let i = 0; i < weight.length; i++) {
